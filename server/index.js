9d4f40c6a277afc7636ac6198162c57b9879ab05
Abraham Esparza <abrahamesparza.us@gmail.com>
2020-12-13 15:45:20 -0800

    connect db to server. GET & POST working in Postman

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const path = require('path');
 const cors = require('cors');
 const port = 3000;
 
+const db = require('../database/db.js');
+const { UserData } = require('../database/index.js');
+// const UserData = require('../database/index.js').UserData;
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -12,6 +16,23 @@ app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).send('OK');
+});
+
+app.post('/financial/data', (req, res) => {
+  let userData = req.body;
+  UserData.create(userData, (err, data) => {
+    if (err) res.status(500).send(err);
+    else res.status(201).send(data)
+  });
+});
+
+
+app.get('/financial/data', (req, res) => {
+  let userData = req.body;
+  UserData.find((err, data) => {
+    if (err) res.status(500).send(err);
+    else res.status(200).send(data);
+  });
 });
 
 app.listen(port, () => {
